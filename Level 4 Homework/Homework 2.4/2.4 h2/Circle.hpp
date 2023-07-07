0a857ed9f07cc429d262dfc076dd6e9d075172a3
Marcus Guerrand <57366572+marcusguerranda@users.noreply.github.com>
2023-07-07 14:47:07 -0400

    Add files via upload

---
+++ Level 4 Homework/Homework 2.4/2.4 h2/Circle.hpp
@@ -0,0 +1,52 @@
+// Circle.hpp 
+// Purpose: Header file of circles defined in 2 dimensions
+// Modification dates: 11/21/2022 
+
+#ifndef Circle_hpp
+#define Circle_hpp
+            
+#include "Point.hpp"            //Incuding Point header file
+#include "Line.hpp"             //Including Line header file  
+#include <sstream>              //C++ stream buffering 
+#include <iostream>
+
+class Circle
+{
+
+//DATA MEMBERS
+
+private:
+        
+        Point m_center;         //Center point
+        double m_radius;        //Radius
+
+public:
+
+// CONSTRUCTORS AND DESTRUCTORS
+
+        Circle();                                           //Default constructor
+        Circle(const Point &center, const double &radius);  //Constructor for circle object taking radius and center point coordinates
+        Circle(const Circle &c);                            //Copy constructor of another circle object
+        Circle& operator = (const Circle &c);               //Assignment operator
+        ~Circle();                                          //Default destructor
+
+// SELECTORS:
+
+        double const &Radius() const;                           //Radius of a said circle
+        Point const &CenterPoint() const;                       //Centerpoint
+
+// MODIFIERS:
+
+        void Radius(const double &radius);                  //Radius function taking as argument a reference value 'radius'.
+        void CenterPoint(const double &center);
+
+        std::string ToString() const;                             //Giving out information on the circle
+        double Diameter() const;                            //Function calculating diameter of circle
+        double Area() const;                                //Function calculating area of circle
+        double Circumference() const;                       //Function calculating circumference of circle
+
+};
+
+std::ostream &operator << (std::ostream& os, const Circle& c);
+
+#endif
